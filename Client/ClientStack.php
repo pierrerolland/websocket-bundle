799c5e2111eab49fecaacef8107b7c96a30431a3
Pierre Rolland <roll.pierre@gmail.com>
2018-01-07 20:33:37 +0100

    Initial commit

---
+++ Client/ClientStack.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace RollandRock\WebsocketBundle\Client;
+
+use Ratchet\ConnectionInterface;
+use RollandRock\WebsocketBundle\Exception\ClientNotFoundException;
+
+/**
+ * @author Pierre Rolland <roll.pierre@gmail.com>
+ */
+class ClientStack
+{
+    /** @var ClientInterface[] */
+    private $clients = [];
+
+    public function addClient(ClientInterface $client)
+    {
+        $this->clients[] = $client;
+    }
+
+    public function getClient(ConnectionInterface $connection): ClientInterface
+    {
+        return $this->clients[$this->getClientIndex($connection)];
+    }
+
+    public function removeClient(ConnectionInterface $connection)
+    {
+        unset($this->clients[$this->getClientIndex($connection)]);
+    }
+
+    private function getClientIndex(ConnectionInterface $connection): int
+    {
+        foreach ($this->clients as $i => $c) {
+            if ($c->getConnection() === $connection) {
+                return $i;
+            }
+        }
+
+        throw new ClientNotFoundException();
+    }
+}
